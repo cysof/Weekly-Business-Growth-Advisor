d93783c97f705dc7d05c04f476524cad6dc251c5
cysof <cp4reallife@yahoo.com>
2025-02-21 15:39:37 +0100

    feat: create integration config file

--- app/config.py
+++ app/config.py
@@ -1,3 +1,49 @@
+import os
+import secrets
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Weekly-Biness-Growth-Advisor"
+    PROJECT_VERSION: str = "0.0.1"
+    PROJECT_DESCRIPTION: str = "API for generating business growth insights"
+    API_PREFIX: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    DEBUG: bool = False
+    TESTING: bool = False
+    Tick_URL: str = os.getenv("Tick_URL", "")
+    TARGET_URL: str = os.getenv("TARGET_URL", "")
+    
+settings = Settings()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import os
 # from pydantic_settings import BaseSettings
 # from dotenv import load_dotenv
@@ -66,3 +112,5 @@
 # def get_settings() -> APISettings:
 #     """Dependency for getting settings."""
 #     return settings
+
+
